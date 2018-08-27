0d4d65030cc46c1b220156ab6a4d494984f3a901
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2018-08-27 23:36:10 +0000

    fixed error with key

--- public/js/script.js
+++ public/js/script.js
@@ -5,7 +5,8 @@ $.ajax({
 	dataType: 'json',
 	type: 'GET',
 	success: function(data) {
-		key = data;
+		key = data[0].googleKey;
+		getData();
 	},
 	error: function(error) {
 		console.log('Error');
@@ -13,20 +14,22 @@ $.ajax({
 	}
 });
 
-$.ajax({
-	url:
-		'https://www.googleapis.com/blogger/v3/blogs/7173052990851751381/posts?key=' +
-		key,
-	dataType: 'json',
-	type: 'GET',
-	success: function(data) {
-		getImg(data);
-	},
-	error: function(error) {
-		console.log('Error');
-		console.log(error);
-	}
-});
+function getData() {
+	$.ajax({
+		url:
+			'https://www.googleapis.com/blogger/v3/blogs/7173052990851751381/posts?key=' +
+			key,
+		dataType: 'json',
+		type: 'GET',
+		success: function(data) {
+			getImg(data);
+		},
+		error: function(error) {
+			console.log('Error');
+			console.log(error);
+		}
+	});
+}
 
 // get first image from each post
 function getImg(firstPage) {
